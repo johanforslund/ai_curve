f88ca6026c5a9fab1e0379588d373584871ba026
Pontus Arnesson <arnesson.pontus@gmail.com>
2020-09-15 21:36:04 +0200

    Added alternative files to run with curve buttons

---
+++ DQN_curve.py
@@ -0,0 +1,327 @@
+import tensorflow as tf      # Deep Learning library
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np           # Handle matrices
+import random                # Handling random number generation
+import time                  # Handling time calculation
+from skimage import transform# Help us to preprocess the frames
+from skimage.color import rgb2gray
+import wandb
+from snake_curve import Game
+
+wandb.init(
+    project="ai-snake",
+    config={
+        "learning_rate": 5e-4,
+        "decay_rate": 0.0005,
+        "gamma": 0.9,
+        "pos_reward": 0.1,
+        "neg_reward": -1,
+        "loss": "mse"
+    }
+)
+
+config = wandb.config
+
+from collections import deque# Ordered collection with ends
+import matplotlib.pyplot as plt # Display graphs
+
+import warnings # This ignore all the warning messages that are normally printed during the training because of skiimage
+warnings.filterwarnings('ignore')
+
+left = [1,0,0]
+right = [0,1,0]
+forward = [0,0,1]
+POSSIBLE_ACTIONS = [left, right, forward]
+NUM_ACTIONS = len(POSSIBLE_ACTIONS)
+
+# Instansiate env
+env = Game(config.pos_reward, config.neg_reward)
+
+def preprocess_frame(frame):
+    # Greyscale frame 
+    gray = rgb2gray(frame)
+    
+    # Normalize Pixel Values
+    normalized_frame = gray/255.0
+    
+    # Resize
+    preprocessed_frame = transform.resize(normalized_frame, [84,84])
+    
+    return preprocessed_frame
+
+stack_size = 4
+
+# Initialize deque with zero-images one array for each image
+stacked_frames  =  deque([np.zeros((84,84), dtype=np.int) for i in range(stack_size)], maxlen=4) 
+
+def stack_frames(stacked_frames, state, is_new_episode):
+    # Preprocess frame
+    frame = preprocess_frame(state)
+    
+    if is_new_episode:
+        # Clear our stacked_frames
+        stacked_frames = deque([np.zeros((84,84), dtype=np.int) for i in range(stack_size)], maxlen=4)
+        
+        # Because we're in a new episode, copy the same frame 4x
+        stacked_frames.append(frame)
+        stacked_frames.append(frame)
+        stacked_frames.append(frame)
+        stacked_frames.append(frame)
+        
+        # Stack the frames
+        stacked_state = np.stack(stacked_frames, axis=2)
+        
+    else:
+        # Append frame to deque, automatically removes the oldest frame
+        stacked_frames.append(frame)
+
+        # Build the stacked state (first dimension specifies different frames)
+        stacked_state = np.stack(stacked_frames, axis=2) 
+    
+    return stacked_state, stacked_frames
+
+### MODEL HYPERPARAMETERS
+state_size = [84,84,4]      # Our input is a stack of 4 frames hence 84x84x4 (Width, height, channels) 
+learning_rate =  config.learning_rate      # Alpha (aka learning rate)
+
+### TRAINING HYPERPARAMETERS
+total_episodes = 10000         # Total episodes for training
+max_steps = 1000             # Max possible steps in an episode
+batch_size = 64            
+
+# Exploration parameters for epsilon greedy strategy
+explore_start = 1.0            # exploration probability at start
+explore_stop = 0.01            # minimum exploration probability 
+decay_rate = config.decay_rate           # exponential decay rate for exploration prob
+
+# Q learning hyperparameters
+gamma = config.gamma                    # Discounting rate
+
+### MEMORY HYPERPARAMETERS
+pretrain_length = batch_size   # Number of experiences stored in the Memory when initialized for the first time
+memory_size = 1000000          # Number of experiences the Memory can keep
+
+### MODIFY THIS TO FALSE IF YOU JUST WANT TO SEE THE TRAINED AGENT
+training = True
+
+## TURN THIS TO TRUE IF YOU WANT TO RENDER THE ENVIRONMENT
+#episode_render = False
+
+class DQNetwork:
+    def __init__(self, state_size, learning_rate, name='DQNetwork'):
+        self.state_size = state_size
+        self.learning_rate = learning_rate
+        self.model = self.build_model()
+
+    def build_model(self):
+        input = keras.Input(shape=self.state_size)
+        x = layers.Conv2D(32, 8, strides=(4,4))(input)
+        x = layers.Activation('relu')(x)
+        x = layers.Conv2D(64, 4, strides=(2,2))(input)
+        x = layers.Activation('relu')(x)
+        x = layers.Conv2D(64, 3, strides=(2,2))(input)
+        x = layers.Activation('relu')(x)
+        x = layers.Flatten()(x)
+        x = layers.Dense(512)(x)
+        x = layers.Activation('relu')(x)
+        output = layers.Dense(NUM_ACTIONS)(x)
+
+        model = keras.Model(input, output)
+        model.compile(optimizer=keras.optimizers.Adam(self.learning_rate), loss=config.loss)
+        return model
+
+dqn = DQNetwork(state_size, learning_rate)
+
+class Memory():
+    def __init__(self, max_size):
+        self.buffer = deque(maxlen = max_size)
+    
+    def add(self, experience):
+        self.buffer.append(experience)
+    
+    def sample(self, batch_size):
+        buffer_size = len(self.buffer)
+        index = np.random.choice(np.arange(buffer_size),
+                                size = batch_size,
+                                replace = False)
+        
+        return [self.buffer[i] for i in index]
+
+# Instantiate memory
+memory = Memory(max_size = memory_size)
+for i in range(pretrain_length):
+    # If it's the first step
+    if i == 0:
+        state = env.reset()
+        state, stacked_frames = stack_frames(stacked_frames, state, True)
+        
+    # Get the next_state, the rewards, done by taking a random action
+    action = random.choice(POSSIBLE_ACTIONS)
+    next_state, reward, terminal = env.step(action)
+    
+    #env.render()
+    
+    # Stack the frames
+    next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+    # If the episode is finished (we're dead 3x)
+    if terminal:
+        # We finished the episode
+        next_state = np.zeros(state.shape)
+        
+        # Add experience to memory
+        memory.add((state, action, reward, next_state, terminal))
+        
+        # Start a new episode
+        state = env.reset()
+        
+        # Stack the frames
+        state, stacked_frames = stack_frames(stacked_frames, state, True)
+        
+    else:
+        # Add experience to memory
+        memory.add((state, action, reward, next_state, terminal))
+        
+        # Our new state is now the next_state
+        state = next_state
+
+def predict_action(explore_start, explore_stop, decay_rate, decay_step, state, actions):
+    ## EPSILON GREEDY STRATEGY
+    # Choose action a from state s using epsilon greedy.
+    ## First we randomize a number
+    exp_exp_tradeoff = np.random.rand()
+
+    # Here we'll use an improved version of our epsilon greedy strategy used in Q-learning notebook
+    explore_probability = explore_stop + (explore_start - explore_stop) * np.exp(-decay_rate * decay_step)
+    
+    if (explore_probability > exp_exp_tradeoff):
+        # Make a random action (exploration)
+        action = random.choice(POSSIBLE_ACTIONS)
+        
+    else:
+        # Get action from Q-network (exploitation)
+        # Estimate the Qs values state
+        Qs = dqn.model.predict(state.reshape((1, *state.shape)))
+        
+        # Take the biggest Q value (= the best action)
+        choice = np.argmax(Qs)
+        action = POSSIBLE_ACTIONS[int(choice)]
+                
+    return action, explore_probability
+
+
+# TRAINING
+if training == True:
+    # Initialize the decay rate (that will use to reduce epsilon) 
+    decay_step = 0
+
+    all_rewards = []
+    
+    for episode in range(total_episodes):
+        # Set step to 0
+        step = 0
+        
+        # Initialize the rewards of the episode
+        episode_rewards = []
+        
+        # Make a new episode and observe the first state
+        state = env.reset()
+        
+        # Remember that stack frame function also call our preprocess function.
+        state, stacked_frames = stack_frames(stacked_frames, state, True)
+        
+        while step < max_steps:
+            step += 1
+            
+            #Increase decay_step
+            decay_step +=1
+            
+            # Predict the action to take and take it
+            action, explore_probability = predict_action(explore_start, explore_stop, decay_rate, decay_step, state, POSSIBLE_ACTIONS)
+            
+            #Perform the action and get the next_state, reward, and done information
+            next_state, reward, terminal = env.step(action)
+            
+            # if episode_render:
+            #     env.render()
+            
+            # Add the reward to total reward
+            episode_rewards.append(reward)
+            
+            # If the game is finished
+            if terminal:
+                # The episode ends so no next state
+                next_state = np.zeros((84,84), dtype=np.int)
+                
+                next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+
+                # Set step = max_steps to end the episode
+                step = max_steps
+
+                # Get the total reward of the episode
+                total_reward = np.sum(episode_rewards)
+
+                all_rewards.append(total_reward)
+
+                print('Episode: {}'.format(episode),
+                                'Total reward: {}'.format(total_reward),
+                                'Explore P: {:.4f}'.format(explore_probability),
+                                'Training Loss {:.4f}'.format(loss))
+
+                # Store transition <st,at,rt+1,st+1> in memory D
+                memory.add((state, action, reward, next_state, terminal))
+
+            else:
+                # Stack the frame of the next_state
+                next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+            
+                # Add experience to memory
+                memory.add((state, action, reward, next_state, terminal))
+
+                # st+1 is now our current state
+                state = next_state
+            
+                           
+                
+            ### LEARNING PART            
+            # Obtain random mini-batch from memory
+            batch = memory.sample(batch_size)
+            states_mb = np.array([each[0] for each in batch], ndmin=3)
+            actions_mb = np.array([each[1] for each in batch])
+            rewards_mb = np.array([each[2] for each in batch]) 
+            next_states_mb = np.array([each[3] for each in batch], ndmin=3)
+            dones_mb = np.array([each[4] for each in batch])
+
+            # Plot stacked_frames
+            #f, axarr = plt.subplots(2,2)
+            #axarr[0,0].imshow(states_mb[0, :, :, 0])
+            #axarr[0,1].imshow(states_mb[0, :, :, 1])
+            #axarr[1,0].imshow(states_mb[0, :, :, 2])
+            #axarr[1,1].imshow(states_mb[0, :, :, 3])
+            #plt.show()
+
+            # Get Q values for next_state
+            Qs_state = dqn.model.predict(states_mb)
+            Qs_next_state = dqn.model.predict(next_states_mb)
+            
+            # Set Q_target = r if the episode ends at s+1, otherwise set Q_target = r + gamma*maxQ(s', a')
+            for i in range(0, len(batch)):
+                terminal = dones_mb[i]
+
+                # If we are in a terminal state, only equals reward
+                if terminal:
+                    #target = rewards_mb[i]
+                    target = rewards_mb[i]
+                    #target_Qs_batch.append(target)
+                    
+                else:
+                    #target = rewards_mb[i] + gamma * np.max(Qs_next_state[i])
+                    target = rewards_mb[i] + gamma * np.max(Qs_next_state[i])
+                
+                Qs_state[i, actions_mb[i].astype(bool)] = target
+                    
+            loss = dqn.model.train_on_batch(states_mb, Qs_state)
+
+        if episode % 10 == 0:
+            print(np.mean(np.array(all_rewards)[-10:]))
+            wandb.log({'Reward': np.mean(np.array(all_rewards)[-10:])})
---
+++ snake_curve.py
@@ -0,0 +1,264 @@
+import math
+import random
+import pygame
+
+WINDOW_SIZE = 500
+ROWS = 12
+START_POS = (6,6)
+
+pygame.init()
+screen = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
+clock = pygame.time.Clock()
+
+training = False
+
+class Game:
+    def __init__(self, pos_reward, neg_reward):
+        self.snake = Snake(START_POS)
+        self.reset()
+        self.high_score = 0
+        self.pos_reward = pos_reward
+        self.neg_reward = neg_reward
+
+    def step(self, action):
+        #pygame.time.wait(200)
+        reward = self.pos_reward
+        terminal = False
+
+        pygame.event.pump()
+
+        self.snake.move(action)
+
+        screen.fill((0,0,0))
+        draw_border()
+
+        self.snake.add_cube()
+
+        terminal = self.snake.check_collision()
+        if terminal:
+            #self.reset()
+            terminal = True
+            reward = self.neg_reward
+            score = len(self.snake.body)
+            if score > self.high_score:
+                self.high_score = score
+            #print('Score: ', score, 'High score: ', self.high_score)
+
+        self.snake.draw()
+
+        pygame.display.update()
+        next_state = pygame.surfarray.array3d(pygame.display.get_surface())
+
+        return (next_state, reward, terminal)
+    
+    def reset(self):
+        self.snake.reset(START_POS)
+        screen.fill((0,0,0))
+        draw_border()
+
+        pygame.display.update()
+        state = pygame.surfarray.array3d(pygame.display.get_surface())
+        return state
+        
+class Snake(object):
+    body = []
+    turns = {}
+    def __init__(self, pos):
+        self.color = (255, 0, 0)
+        self.head = Cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 1
+        self.dirny = 0
+
+    def move(self, action):
+        if action == None:
+            pass
+        elif action[0] == 1:
+            if self.dirnx == -1 and self.dirny == 0: # Left
+                self.dirnx = 0
+                self.dirny = 1
+            elif self.dirnx == 0 and self.dirny == -1: # Up
+                self.dirnx = -1
+                self.dirny = 0
+            elif self.dirnx == 1 and self.dirny == 0: # Right
+                self.dirnx = 0
+                self.dirny = -1
+            elif self.dirnx == 0 and self.dirny == 1: # Down
+                self.dirnx = 1
+                self.dirny = 0
+            self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        elif action[1] == 1:
+            if self.dirnx == 1 and self.dirny == 0: # Right
+                self.dirnx = 0
+                self.dirny = 1
+            elif self.dirnx == 0 and self.dirny == -1: # Up
+                self.dirnx = 1
+                self.dirny = 0
+            elif self.dirnx == -1 and self.dirny == 0: # Left
+                self.dirnx = 0
+                self.dirny = -1
+            elif self.dirnx == 0 and self.dirny == 1: # Down
+                self.dirnx = -1
+                self.dirny = 0
+            self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        elif action[2] == 1:
+            pass
+        # elif action[2] == 1:
+        #     self.dirnx = 0
+        #     self.dirny = -1
+        #     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        # elif action[3] == 1:
+        #     self.dirnx = 0
+        #     self.dirny = 1
+        #     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+        # if action == 0:
+        #     self.dirnx = -1
+        #     self.dirny = 0
+        #     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        # elif action == 1:
+        #     self.dirnx = 1
+        #     self.dirny = 0
+        #     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        # elif action == 2:
+        #     self.dirnx = 0
+        #     self.dirny = -1
+        #     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        # elif action == 3:
+        #     self.dirnx = 0
+        #     self.dirny = 1
+        #     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]               
+
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0],turn[1])
+                if i == len(self.body)-1:
+                    self.turns.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (ROWS-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= ROWS-1: c.pos = (0,c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= ROWS-1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],ROWS-1)
+                else: c.move(c.dirnx,c.dirny)
+
+    def reset(self, pos):
+        self.head = Cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 1
+        self.dirny = 0
+
+    def add_cube(self):
+        tail = self.body[-1]
+        dx, dy = tail.dirnx, tail.dirny
+
+        if dx == 1 and dy == 0:
+            self.body.append(Cube((tail.pos[0]-1,tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(Cube((tail.pos[0]+1,tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(Cube((tail.pos[0],tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(Cube((tail.pos[0],tail.pos[1]+1)))
+
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
+        
+
+    def draw(self):
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(eyes=True)
+            else:
+                c.draw()
+
+    def check_collision(self):
+        for x in range(len(self.body)):
+            if self.body[x].pos in list(map(lambda z:z.pos, self.body[x+1:])):
+                return True
+
+        if self.head.pos[0] == ROWS-1 or self.head.pos[0] == 0 or self.head.pos[1] == ROWS -1 or self.head.pos[1] == 0:
+            return True
+
+        return False
+
+class Cube(object):
+    def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
+        self.pos = start
+        self.dirnx = 1
+        self.dirny = 0
+        self.color = color
+
+    def move(self, dirnx, dirny):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+
+    def draw(self, eyes=False):
+        dis = WINDOW_SIZE // ROWS
+        i = self.pos[0]
+        j = self.pos[1]
+
+        pygame.draw.rect(screen, self.color, (i*dis+1,j*dis+1, dis-2, dis-2))
+        if eyes:
+            centre = dis//2
+            radius = 3
+            circleMiddle = (i*dis+centre-radius,j*dis+8)
+            circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
+            pygame.draw.circle(screen, (0,0,0), circleMiddle, radius)
+            pygame.draw.circle(screen, (0,0,0), circleMiddle2, radius)
+
+def draw_grid():
+    cell_size = WINDOW_SIZE // ROWS
+
+    x = 0
+    y = 0
+    for l in range(ROWS):
+        x = x + cell_size
+        y = y + cell_size
+
+        pygame.draw.line(screen, (255,255,255), (x,0),(x, WINDOW_SIZE))
+        pygame.draw.line(screen, (255,255,255), (0,y),(WINDOW_SIZE, y))
+
+def draw_border():
+    cell_size = WINDOW_SIZE // ROWS
+    
+    x = 0
+    y = 0
+    for l in range(ROWS):
+        x = x + cell_size
+        y = y + cell_size
+
+        wall1 = Cube((l, 0), color=(0, 255, 0))
+        wall2 = Cube((0, l), color=(0, 255, 0))
+        wall3 = Cube((ROWS-1, l), color=(0, 255, 0))
+        wall4 = Cube((l, ROWS-1), color=(0, 255, 0))
+        wall1.draw()
+        wall2.draw()
+        wall3.draw()
+        wall4.draw()
+
+def play_controller(game):
+    while True:
+        pygame.event.pump()
+
+        for event in pygame.event.get():
+            keys = pygame.key.get_pressed()
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    action = [1, 0, 0]
+                elif keys[pygame.K_RIGHT]:
+                    action = [0, 1, 0]
+                # elif keys[pygame.K_UP]:
+                #     action = [0, 0, 1, 0]
+                # elif keys[pygame.K_DOWN]:
+                #     action = [0, 0, 0, 1]
+                else:
+                    action = None
+        game.step(action)
+       
+#game = Game()
+#play_controller(game)
